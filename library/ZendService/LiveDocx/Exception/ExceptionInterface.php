f5a5c27ebe2be197188ad5ecf70d11b68de69a8e
prolic <saschaprolic@googlemail.com>
2012-06-26 23:04:00 +0200

    [zen-52] Renamed abstract classes in Zend\Service
    Renamed abstract classes
    Fixed exceptions
    Renamed interfaces
    Fixed phpdoc

--- library/ZendService/LiveDocx/Exception/ExceptionInterface.php
+++ library/ZendService/LiveDocx/Exception/ExceptionInterface.php
@@ -19,7 +19,7 @@
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
-namespace Zend\Service\LiveDocx;
+namespace Zend\Service\LiveDocx\Exception;
 
 /**
  * @category   Zend
@@ -28,7 +28,6 @@ namespace Zend\Service\LiveDocx;
  * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-interface Exception
+interface ExceptionInterface
 {
-
 }
